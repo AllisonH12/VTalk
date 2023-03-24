ff3f5f5d25d96503e63f5cdd2f2de657fd1414d5
Allison Huang <111381124+AllisonH12@users.noreply.github.com>
2023-03-24 11:28:57 -0700

    Update replic.py

--- replic.py
+++ replic.py
@@ -6,7 +6,7 @@ version = model.versions.get("50adaf2d3ad20a6f911a8a9e3ccf777b263b8596fbd2c8fc26
 # https://replicate.com/methexis-inc/img2prompt/versions/50adaf2d3ad20a6f911a8a9e3ccf777b263b8596fbd2c8fc26e8888f8a0edbb5#input
 inputs = {
     # Input image
-    'image': open("/Users/yonghuang/code/VTalk/images/anger.jpeg", "rb"),
+    'image': open("/Users/al/code/VTalk/images/anger.jpeg", "rb"),
 }
 
 # https://replicate.com/methexis-inc/img2prompt/versions/50adaf2d3ad20a6f911a8a9e3ccf777b263b8596fbd2c8fc26e8888f8a0edbb5#output-schema
